2b4c605577d3c6f56ab252bd066e0756a9c01655
mauloredo <maurilorepina@gmail.com>
2025-02-22 17:17:20 -0600

    Se añaden notas previas al código

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//crear un array para almacenar los nombres
+//implementar una función para agregar amigos
+//implementar una función para sortear amigos
+//realizar Read.me
+//Identificar objetos en el archivo html
+    //button input-wrapper con la leyenda "agregar"
+    //funcion "agregarAmigo" sobre el button "agregar
+    // elemento ul para el display de la lista de nombres. Para este elemento se utiliza li para añadir en la lista
+    //el array tiene el tagID de listaAmigos
+    //button input-container con el display "sortear amigo segreto"
+    //funcion "sortearAmigo sobre el button "sortear amigo secreto"
+    
