8a95c7906aff821f174dd6384bf7231151f4cf70
mauloredo <maurilorepina@gmail.com>
2025-02-22 20:09:27 -0600

    Se añaden los cuatro pasos para la función agregar amigos

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//crear un array para almacenar los nombres
-//implementar una función para agregar amigos
+
+
+//1.          crear un array para almacenar los nombres
+let listaCinco = []
+//Como resultado de este paso nos dará la lista listaCinco, la cual se utilizará para el siguiente paso
+
+//2.          implementar una función para agregar amigos. Se utilizan los elementos:
+//input box amigo
+//función "agregar amigo"
+//elemento ul para el display de los nombres
+//Tag ID nombreAmigo
+        //2.1 Capturar el valor del campo de entrada: Utilizar document.getElementById o 
+        // document.querySelector para obtener el texto ingresado por el usuario.
+        function agregarAmigo() {
+        const nombreAmigo = document.getElementById(amigo).nodeValue;
+        //2.2Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+
+//Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+
+//Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
+}
+
+
+
 //implementar una función para sortear amigos
 //realizar Read.me
 //Identificar objetos en el archivo html
     //button input-wrapper con la leyenda "agregar"
     //funcion "agregarAmigo" sobre el button "agregar
-    // elemento ul para el display de la lista de nombres. Para este elemento se utiliza li para añadir en la lista
+    // elemento ul para el display de la lista de nombres. 
+    // Para este elemento se utiliza li para añadir en la lista
     //el array tiene el tagID de listaAmigos
     //button input-container con el display "sortear amigo segreto"
     //funcion "sortearAmigo sobre el button "sortear amigo secreto"
-    
+
+    //LISTA DE FUNCIONES
+    //agregarAmigo
+
+
+    //LISTA DE VARIABLES
+    //input box amigo
+    //listaCinco es el nombre del array
+    //nombreAmigo es el tagID de los nombres de los Amigos
+ 
